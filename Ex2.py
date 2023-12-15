5868fd1fd99d19a4df76fda01a4264f353470273
Fgoldarlar <fgoldarlar@educacion.navarra.es>
2023-12-15 11:14:34 +0100

    Update Ex2.py

--- Ex2.py
+++ Ex2.py
@@ -7,13 +7,14 @@ def MostrarMultiplicar ():
     Debe mostrarlo por pantalla. Si el fichero no existe debe mostrar un 
     mensaje por pantalla informando de ello.
     """
-n = int(input("Dime un numero entero del 1 al 10"))
-if 1 <= n <= 10:
-    try:
-        fichero = "Tabla-" + str(n) + ".txt"
-        with open(fichero, "r") as archivo: 
-            print("El archivo se ha leido!")
-    except FileNotFoundError:
-        print("No está el fichero")
-else:
-    print("Venga gracioso, que te he dicho del 1 al 10")
+    n = int(input("Dime un numero entero del 1 al 10"))
+    if 1 <= n <= 10:
+        try:
+            fichero = "Tabla-" + str(n) + ".txt"
+            with open(fichero, "r") as archivo: 
+                print("El archivo se ha leido!")
+        except FileNotFoundError:
+            print("No está el fichero")
+    else:
+        print("Venga gracioso, que te he dicho del 1 al 10")
+    return
